7ee23bde01ba33cbc9b88d5ac7f5ab0c6058c18d
unknown <developer1618@gmail.com>
2024-03-18 12:27:06 +0500

    Debug borders

--- src/script.js
+++ src/script.js
@@ -147,7 +147,8 @@ function showResults() {
   const percentage = (score / quizData.length) * 100;
   let resultHTML = `
     <div class="flex justify-center">
-      <div class="container h-36 quiz-timeline border border-[#3E3E70] py-2 px-8 rounded-2xl max-w-xl flex justify-center items-center align-middle bg-white mb-4">
+      <div
+        class="container h-36 quiz-timeline border border-[#3E3E70] py-2 px-8 rounded-2xl max-w-xl flex justify-center items-center align-middle bg-white mb-4">
         <div class="text-center px-8">
           <h3 class="text-[#3E3E70] font-medium">Your results:</h3>
           <h2 class="text-[#3E3E70] font-semibold text-3xl">${percentage.toFixed(0)}%</h2>
@@ -162,31 +163,50 @@ function showResults() {
       </div>
     </div>
   `;
-
+  resultHTML += `
+  <div class="flex justify-between">
+      <button onclick="location.reload()"
+          class="flex justify-center w-full items-center bg-white border border-[#3E3E70] transition duration-700 text-[#3E3E70] hover:bg-[#3E3E70] hover:text-white px-4 py-2 rounded-2xl mb-8 mr-8">Take
+          the test again
+          <span class="pl-2">
+            <?xml version="1.0" encoding="iso-8859-1"?>
+            <!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+            <svg height="16px" width="16px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+              xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 512 512" xml:space="preserve">
+              <path style="fill:#3E3E70;" d="M69.816,256H0l93.096,93.096L186.2,256h-69.816c0.224-77.016,62.6-139.4,139.616-139.632
+              c22.672,0.432,44.952,6,65.16,16.296l34.896-34.896C325.6,80.144,291.176,70.528,256,69.832
+              C153.296,70.112,70.104,153.296,69.816,256z M395.616,256c-0.224,77.016-62.6,139.4-139.616,139.632
+              c-22.672-0.432-44.952-6-65.16-16.296l-34.896,34.896c30.456,17.624,64.88,27.24,100.056,27.936
+              C358.696,441.872,441.88,358.696,442.184,256H512l-93.096-93.096L325.8,256H395.616z" />
+            </svg>
+          </span></button>
+        <a href="start.html"
+          class="flex justify-center w-full bg-white border border-[#3E3E70] transition duration-700 text-[#3E3E70] hover:bg-[#3E3E70] hover:text-white px-4 py-2 rounded-2xl mb-8">Go
+          to homepage</a>
+      </div>
+  `;
   for (let i = 0; i < quizData.length; i++) {
     const userAnswer = userAnswers[i];
     const currentQuizData = quizData[i];
-    const isCorrect = userAnswer && userAnswer.isCorrect;
-    const borderClass = isCorrect ? 'border-green-500' : 'border-red-500'; // Определяем класс границы в зависимости от правильности ответа
+    const isCorrect = userAnswer && userAnswer.answer === currentQuizData.correct;
+    const userAnswerText = userAnswer ? currentQuizData[userAnswer.answer] : 'not answered';
+    const userAnswerColor = isCorrect ? 'text-green-500' : 'text-red-500';
+    const borderClass = isCorrect ? 'border-green-500' : 'border-red-500';
     resultHTML += `
       <div class="grid justify-center">
-        <div class="quiz-question__block border p-4 rounded-2xl bg-white mb-8 ${borderClass}">
+        <div class="quiz-question__block border ${borderClass} p-4 rounded-2xl bg-white mb-8">
           <h3 class="font-semibold pb-4">${i + 1}. ${currentQuizData.question}</h3>
           <h4 class="text-red-600 font-semibold pb-2">
-          <span class="${isCorrect ? 'text-green-500' : 'text-red-500'}">Your answer: ${userAnswer ? currentQuizData[userAnswer.answer] : 'not answered'}</span>
-          ${isCorrect ? '' : `<br><span class="text-green-500">Correct answer: ${currentQuizData[currentQuizData.correct]}</span>`}
-          </h4>
+          <span class="${userAnswerColor}">Your answer: ${userAnswerText}</span>${isCorrect ? '' : `<br> <span class="text-green-500">Correct answer: ${currentQuizData[currentQuizData.correct]}</span>`}</h4>
         </div>
       </div>
     `;
   }
-
   quiz.innerHTML = resultHTML;
   // Скрыть quiz-timelines
   const quizTimelines = document.getElementById('quiz-timelines');
   quizTimelines.style.display = 'none';
 }
-
 
 function disableSubmitButton() {
   submitBtn.disabled = true;
@@ -265,4 +285,3 @@ function createQuestionNumbers() {
     quizTimeline.appendChild(span);
   }
 }
-
